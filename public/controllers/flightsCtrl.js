fb0fc6046db0724f6f9816e8df4fb4d979d72819
George Maged <georgemaged9129@gmail.com>
2016-04-04 00:15:02 +0200

    Now it says the correct origin and destination airports

--- public/controllers/flightsCtrl.js
+++ public/controllers/flightsCtrl.js
@@ -1,5 +1,8 @@
-App.controller('flightsCtrl',function($scope,$location){
+App.controller('flightsCtrl',function($scope,FlightsSrv,$location){
     $scope.return = function() {
     $location.url('/flights2');
     };
+
+    $scope.origin = FlightsSrv.getSelectedOriginAirport();
+    $scope.destination = FlightsSrv.getSelectedDestinationAirport();
 })
