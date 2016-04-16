260493e7d91f59db7c42e6b9a9c4119596ee82cc
George Maged <georgemaged9129@gmail.com>
2016-04-16 22:02:30 +0200

    Fixed .env

--- app/routes.js
+++ app/routes.js
@@ -54,71 +54,86 @@ module.exports = function(app,mongo) {
 	
 
     }); 
-
-    /* ROUND-TRIP SEARCH REST ENDPOINT */
-    app.get('/api/flights/search/:origin/:destination/:departingDate/returningDate/:class', function(req, res) {
-        // retrieve origin, destination, departingDate, returningDate, and class from req.params.{{origin | departingDate | ...}}
-        // return an array of objects with this exact format
+    /**
+     * ROUND-TRIP SEARCH REST ENDPOINT
+     * @param origin - Flight Origin Location - Airport Code
+     * @param destination - Flight Destination Location - Airport Code
+     * @param departingDate - JavaScript Date.GetTime() numerical value corresponding to format `YYYY-MM-DD`
+     * @param returningDate - JavaScript Date.GetTime() numerical value corresponding to format `YYYY-MM-DD`
+     * @param class - economy or business only
+     * @returns {Array}
+     */        
+    app.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate/:class', function(req, res) {
+        // retrieve params from req.params.{{origin | departingDate | ...}}
+        // return this exact format
         return 
-        [{
-            "flightNumber"      : "SE2804",
-            "aircraftType"      : "Boeing",
-            "aircraftModel"     : "747",
-            "departureDateTime" : "Tuesday, April 12, 2016 06:25 PM",
-            "arrivalDateTime"   : "Wednesday, April 13, 2016 12:25 AM",
-            "origin"            : "JFK",
-            "destination"       : "CAI",
-            "cost"              : "750",
-            "currency"          : "USD",
-            "class"             : "economy",
-            "Airline"           : "United"
-        },{
-            "flightNumber"      : "SE2805",
-            "aircraftType"      : "Boeing",
-            "aircraftModel"     : "747",
-            "departureDateTime" : "Friday, April 23, 2016 04:25 AM",
-            "arrivalDateTime"   : "Friday, April 23, 2016 03:25 PM",
-            "origin"            : "CAI",
-            "destination"       : "JFK",
-            "cost"              : "845",
-            "currency"          : "USD",
-            "class"             : "economy",
-            "Airline"           : "United"
-        }];
+        {
+          outgoingFlights: 
+            [{
+                "flightNumber"      : "SE2804",
+                "aircraftType"      : "Boeing",
+                "aircraftModel"     : "747",
+                "departureDateTime" : 1460478300000,
+                "arrivalDateTime"   : 1460478300000,
+                "origin"            : "JFK",
+                "destination"       : "CAI",
+                "cost"              : "750",
+                "currency"          : "USD",
+                "class"             : "economy",
+                "Airline"           : "United"
+            }
+
+            // ,
+            // {
+            //     other flights
+            // }
+            ]
+           
+          returnFlights:
+            [{
+                "flightNumber"      : "SE2805",
+                "aircraftType"      : "Boeing",
+                "aircraftModel"     : "747",
+                "departureDateTime" : 1460478300000,
+                "arrivalDateTime"   : 1460478300000,
+                "origin"            : "CAI",
+                "destination"       : "JFK",
+                "cost"              : "845",
+                "currency"          : "USD",
+                "class"             : "economy",
+                "Airline"           : "United"
+            }]
+        };
     });    
 
-    /* ROUND-TRIP SEARCH REST ENDPOINT */
-    app.post('/api/flights/search/roundtrip', function(req, res) {
-        // retrieve origin, destination, departingDate, returningDate, and class from req.payload
-        // return an array of objects matching format above
-        return [{}];
-    });     
+    /**
+     * ONE-WAY SEARCH REST ENDPOINT 
+     * @param origin - Flight Origin Location - Airport Code
+     * @param DepartingDate - JavaScript Date.GetTime() numerical value corresponding to format `YYYY-MM-DD`
+     * @param class - economy or business only
+     * @returns {Array}
+     */    
+    app.get('/api/flights/search/:origin/:destination/:departingDate/:class', function(req, res) {
+        // retrieve params from req.params.{{origin | departingDate | ...}}
+        // return this exact format
 
-    /* ONE-WAY SEARCH REST ENDPOINT */
-    app.get('/api/flights/search/:origin/:departingDate/:class', function(req, res) {
-        // retrieve origin, destination, departingDate, and class from req.params.{{origin | departingDate}}
-        // return an array of objects with this exact format
         return 
-        [{
-            "flightNumber"      : "SE2804",
-            "aircraftType"      : "Airbus",
-            "aircraftModel"     : "A320",
-            "departureDateTime" : "Tuesday, April 12, 2016 06:25 PM",
-            "arrivalDateTime"   : "Wednesday, April 13, 2016 12:25 AM",
-            "origin"            : "JFK",
-            "destination"       : "CAI",
-            "cost"              : "1567",
-            "currency"          : "USD",
-            "class"             : "economy",
-            "Airline"           : "United"
-        }];    
+        {
+          outgoingFlights: 
+            [{
+                "flightNumber"      : "SE2804",
+                "aircraftType"      : "Airbus",
+                "aircraftModel"     : "A320",
+                "departureDateTime" : 1460478300000,
+                "arrivalDateTime"   : 1460478300000,
+                "origin"            : "JFK",
+                "destination"       : "CAI",
+                "cost"              : "1567",
+                "currency"          : "USD",
+                "class"             : "economy",
+                "Airline"           : "United"
+            }]
+        };
     });        
-
-    /* ONE-WAY SEARCH REST ENDPOINT */
-    app.post('/api/flights/search/oneway', function(req, res) {
-        // retrieve origin, destination, departingDate, and class from req.payload
-        // return an array of objects matching format above
-        return [{}];
-    });         
-
+             
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app     = require('./app/app');
 
 app.listen('3000', function(){
-	// require('./app/db').init(process.env.MONGODB_URL);
+	require('./app/db').init(process.env.MONGODB_URL);
   	console.log('[OK] => HTTP Server listening on http://localhost:3000');
 });
