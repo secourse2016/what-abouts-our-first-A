bcf6882f15fbc8206ea0d90537cc080fff81125a
Marc19 <marciskander94@gmail.com>
2016-04-17 23:57:53 +0200

    started working on reservationsCtrl

--- app/allFlights.js
+++ app/allFlights.js
@@ -129,3 +129,4 @@ function viewMyReservedFlight( bookingRefNum , cb ){
         
     });
 }
+
--- public/controllers/reservationsCtrl.js
+++ public/controllers/reservationsCtrl.js
@@ -5,15 +5,16 @@ App.controller('reservationsCtrl', function($scope,FlightsSrv,$location) {
 	$scope.search = function()
 	{
 		FlightsSrv.setSelectedBookingRefNumber($scope.selectedBookingRefNumber);
-		FlightsSrv.getMyReservation.success(function(reservation){
-			$scope.origin = reservation.origin ;
-			$scope.destination = reservation.destination ;
-			$scope.cabin = reservation.
-			$scope.flightNumber = reservation.flightNumber ;
-			$scope.seatNumber = reservation.
-			$scope.duration = reservation.duration ;
-			$scope.departureTime = reservation.date ;
-			// $scope.arrivalTime = reservation.
+		FlightsSrv.getMyReservation.success(function(reservations){
+			$scope.origin = reservation[0].origin ;
+			$scope.destination = reservation[0].destination ;
+			$scope.cabin = "bolla";//reservation[0]
+			$scope.flightNumber = reservation[0].flightNumber ;
+			$scope.seatNumber = 2;//reservation[0].
+			$scope.duration = reservation[0].duration ;
+			$scope.departureTime = reservation[0].date ;
+			var arrivingTime = reservation[0].destination + reservation[0].duration
+			$scope.arrivalTime = arrivingTime;
 		});
 		$scope.isCollapsed = !$scope.isCollapsed;
 	}
